42055e9ee5c0628a7803dd3118bf1744b2d74b8c
加藤樹\itsuk <i.kato@h-company.co.jp>
2025-07-16 11:01:01 +0900

    コメント追記・不要なimportの削除

--- backend/src/message/graphql-types/input/create-message.input.ts
+++ backend/src/message/graphql-types/input/create-message.input.ts
@@ -5,9 +5,11 @@ export class CreateMessageInput {
   @Field(() => String)
   message: string;
 
+  // TODO：リレーション定義追加後にnot nullに変更
   @Field(() => Int, { nullable: true })
   userId: number;
 
+  // TODO：リレーション定義追加後にnot nullに変更
   @Field(() => Int, { nullable: true })
   channelId: number;
 }
--- backend/src/message/message.module.ts
+++ backend/src/message/message.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { MessageResolver } from './message.resolver';
 import { MessageDao } from './dao/message.dao';
 import { PrismaModule } from 'src/shared/prisma/prisma.module';
-import { PubSubModule } from 'src/shared/pubsub/pubsub.module';
 
 @Module({
   imports: [PrismaModule],
