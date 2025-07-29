dbfba1916bfa45e5710fb22aecf02972dd79d9c2
加藤樹\itsuk <i.kato@h-company.co.jp>
2025-07-29 22:46:37 +0900

    ヘッダーの色変更

--- frontend/components/header/header.tsx
+++ frontend/components/header/header.tsx
@@ -1,6 +1,6 @@
 export const Header = () => {
   return (
-    <div className="fixed flex justify-between px-8 w-screen h-16 bg-slate-800 items-center drop-shadow-2xl border-b border-gray-300 shadow-md">
+    <div className="fixed flex justify-between px-8 w-screen h-16 bg-primary items-center drop-shadow-2xl border-b border-gray-300 shadow-md">
       <h1 className="font-bold text-2xl text-white">リアルタイムチャット！</h1>
     </div>
   );
